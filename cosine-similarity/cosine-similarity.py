bc98b3f304f49efe2717dfb71decdfe8a6b8d5a2
Raymond Wu <66080982+carloburger@users.noreply.github.com>
2026-09-22 17:00:09 -0400

    Solve cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -7,7 +7,7 @@ def cosine_similarity(a: list, b: list) -> float:
     # Write code here
     a_arr = np.asarray(a, dtype=float)
     b_arr = np.asarray(b, dtype=float)
-    ab_dot = np.dot(a, b)
+    ab_dot = np.dot(a_arr, b_arr)
     a_norm = np.linalg.norm(a_arr)
     b_norm = np.linalg.norm(b_arr)
 
